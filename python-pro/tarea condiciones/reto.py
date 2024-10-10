903219a17dfbab89489c55047c0920538e2cac41
Andrés del Pino <114906845+0Vinylo0@users.noreply.github.com>
2024-10-10 05:13:48 +0200

    Subida automática de archivo

---
+++ python-pro/tarea condiciones/reto.py
@@ -0,0 +1,8 @@
+numero = int(input("Adivinaaaaa: "))
+
+while numero <= 100:
+    print ("no hacertaste ;)")
+    numero = int(input("Adivinaaaaaa: "))
+
+if numero > 100:
+    print ("felicidades saliste de videojuego")
