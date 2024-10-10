8b332a35ea998632218393ba2713ca370ccf77ae
Andrés del Pino <114906845+0Vinylo0@users.noreply.github.com>
2024-10-10 09:41:11 +0200

    Actualización automática del archivo

--- python-pro/tarea mas bucles/5_promedio_calificaciones.py
+++ python-pro/tarea mas bucles/5_promedio_calificaciones.py
@@ -8,6 +8,6 @@ for i in alumnos:
     print(f"{i} tiene un {notas[contador]}")
     contador += 1
 suma_calificacion = 0
-for i in range(alumnos):
+for i in range(len(alumnos)):
     suma_calificacion += notas[i]
-print(f"Nota media es: {suma_calificacion / alumnos}")
+print(f"Nota media es: {suma_calificacion / len(alumnos)}")
