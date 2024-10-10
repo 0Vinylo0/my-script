73d40c81725584519d9a8e6435779b7411d4997e
Andrés del Pino <114906845+0Vinylo0@users.noreply.github.com>
2024-10-10 08:51:08 +0200

    Actualización automática del archivo

--- python-pro/tarea mas bucles/2_registro_asistencias.py
+++ python-pro/tarea mas bucles/2_registro_asistencias.py
@@ -1,9 +1,20 @@
-lista = ["pedro", "juan", "manola"]
+#lista = ["pedro", "juan", "manola"]
+#for i in range(0,len(lista),1):
+#    print(f"{lista[i]}")
+#    hola = input("presente, si o no: ")
+#    if hola == "si":
+#        lista[i] = lista[i] + ": presente"
+#    elif hola == "no":
+#        lista[i] = lista[i] + ": no presente"
+#print(f"{lista}")
+
+lista = ["pedro", "juan", "manolo"]
 for i in range(0,len(lista),1):
-    print(f"{lista[i]}")
-    hola = input("presente, si o no: ")
-    if hola == "si":
-        lista[i] = lista[i] + ": presente"
-    elif hola == "no":
-        lista[i] = lista[i] + ": no presente"
-print(f"{lista}")
+    alumno = i
+    print(f"{alumno}")
+    input("presente, si o no: ")
+    if alumno == "si":
+        lista.append(f"{alumno}: presente")
+    elif alumno == "no":
+        lista.append(f"{alumno}: no presente")
+print (f"{alumno}")
