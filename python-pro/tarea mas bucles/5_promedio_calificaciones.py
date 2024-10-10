5848de4a12a9fac6f5873cd8255195b9b66a1a9d
Andrés del Pino <114906845+0Vinylo0@users.noreply.github.com>
2024-10-10 09:25:11 +0200

    Actualización automática del archivo

--- python-pro/tarea mas bucles/5_promedio_calificaciones.py
+++ python-pro/tarea mas bucles/5_promedio_calificaciones.py
@@ -4,4 +4,4 @@ for i in alumnos:
     calificacion = input(f"Que nota tiene {i}?")
     notas.append(calificacion)
 for i in alumnos:
-    print(f"{i} tiene un {notas}")
+    print(f"{i} tiene un {notas[len(i)]}")
