a3a9ba17349273feabd8d4c80e165288e311ad57
Andrés del Pino <114906845+0Vinylo0@users.noreply.github.com>
2024-10-10 05:13:44 +0200

    Subida automática de archivo

---
+++ python-pro/tarea condiciones/ejercicio5.py
@@ -0,0 +1,19 @@
+pedir_dia = input("Que dia es hoy?: ")
+
+if pedir_dia == "lunes":
+    print("mala suete compañero, te espero al final")
+
+elif pedir_dia == "viernes":
+    print("felicidades llegaste al final, pollo para cenar")
+    
+elif pedir_dia == "sabado":
+    print("que feliz el sabado siempre de fiesta ;-)")
+
+elif pedir_dia == "domingo":
+    print("descansa que mañana toca :(")
+
+elif pedir_dia == ("martes" or "miercoles" or "jueves"):
+    print("ya queda poco compañero :-)")
+
+else:
+    print("dime un dia no tonterias porfa :3")
