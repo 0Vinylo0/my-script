195b454c4f87091196d6fdde2ec903706c252041
Andrés del Pino <114906845+0Vinylo0@users.noreply.github.com>
2024-10-10 09:39:11 +0200

    Actualización automática del archivo

--- python-pro/tarea mas bucles/5_promedio_calificaciones.py
+++ python-pro/tarea mas bucles/5_promedio_calificaciones.py
@@ -1,7 +1,7 @@
 alumnos = ["juan", "pepe", "maria", "juancarlos", "paco", "candela"]
 notas = []
 for i in alumnos:
-    calificacion = input(f"Que nota tiene {i}?")
+    calificacion = int(input(f"Que nota tiene {i}?"))
     notas.append(calificacion)
 contador = 0
 for i in alumnos:
