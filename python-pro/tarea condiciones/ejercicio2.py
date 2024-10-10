c3d45cab9ff442bd1214d1072924ecc4d758ed95
Andrés del Pino <114906845+0Vinylo0@users.noreply.github.com>
2024-10-10 05:13:50 +0200

    Subida automática de archivo

---
+++ python-pro/tarea condiciones/ejercicio2.py
@@ -0,0 +1,8 @@
+primer_nuemro = int(input("Dime tu primer numero: "))
+segundo_numero = int(input("Dime tu segundo numero: "))
+
+if primer_nuemro > segundo_numero:
+    print(f"{primer_nuemro} es mayor que {segundo_numero}")
+
+elif segundo_numero > primer_nuemro:
+    print(f"{primer_nuemro} es menor que {segundo_numero}")
